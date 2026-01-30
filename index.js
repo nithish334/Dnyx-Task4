8113157f9f25cdf083649d8b674a5426bbcb4a07
nithish334 <kumnithish122@gmail.com>
2026-01-30 21:56:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const connectDB = require("./config/db");
-connectDB();
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
-
-dotenv.config();
-connectDB();
+import express from "express";
+import mongoose from "mongoose";
+import noteRoutes from "./routes/notes.js";
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
-app.use("/api/notes", require("./routes/notes"));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/notes", noteRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Simple Notes API Running");
+  res.send("Notes API is running");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
