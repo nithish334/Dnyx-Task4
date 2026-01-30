3504c7812cdb8cd2f1db206fa0a59b8239673a01
nithish334 <kumnithish122@gmail.com>
2026-01-31 00:04:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();   // 🔑 loads .env file
+dotenv.config();
 
 const app = express();
 
-// 🔗 MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.error(err));
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running successfully 🚀");
+});
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
-export default app;
 
+export default app;
